5c4fc41e679b6557b78e5506148d3c1036e61a38
Erik Trom <erik.trom@gmail.com>
2013-12-06 01:20:04 -0800

    explain how promises work, use Ember.RSVP.hash to resolve data from two sources inside model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,30 @@
+function getJSON(something) {
+  console.log("something is: ", something);
+  return new Ember.RSVP.Promise(function (resolve, reject) {
+    if (something == 'weather.json') {
+      setTimeout(function () {
+        console.log('from inside weather.json option')
+        resolve('some weather data');
+      }, 2000);
+    }
+
+    if (something == 'image.json') {
+      setTimeout(function () {
+        resolve('some image data');
+      }, 500);
+    }
+  });
+}
+
+
 export default Ember.Route.extend({
   model: function () {
-    // later we'll use this hook to grab weather data from *somewhere*
-    // or we'll realize that the index route hook won't allow query params
-    // so we'll move this.
-    return {
-      weatherData: 'some weather data from <some weather api>',
-      imageData: 'some data returned by <insert your fav image api here>'
+
+    var promises = {
+      weatherData: getJSON('weather.json'),
+      imageData: getJSON('image.json')
     };
+
+    return Ember.RSVP.hash(promises);
   }
 });
