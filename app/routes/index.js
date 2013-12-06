283149e24bfe247a9221d511124da60d220c3884
Erik Trom <erik.trom@gmail.com>
2013-12-06 10:50:29 -0800

    make ajax run loop aware; abstract out settings object

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,20 +4,49 @@ import apiKeys from 'appkit/utils/api-keys';
 import fakeWeatherData from 'appkit/utils/fake-weather-data';
 
 var FAKEWEATHERSERVER = true;
+var currentWeatherUrl = 'http://api.wunderground.com/api/' +
+                        apiKeys.wunderground +
+                        '/geolookup/conditions/q/WA/Seattle.json';
 
-function getWeatherData() {
+function getCurrentWeatherData() {
+  // TODO: use local storage to keep a incrementing counter, every 15 reloads,
+  // get real data, aka, set this to false, for a sanity check
+  // OR
+  // stub out use sinon
+  // OR
+  // ...
   if (FAKEWEATHERSERVER) { return fakeWeatherData; }
 
   return new Ember.RSVP.Promise(function (resolve, reject) {
-    Ember.$.ajax({
-      url : 'http://api.wunderground.com/api/' + apiKeys.wunderground + '/geolookup/conditions/q/WA/Seattle.json',
+
+    var settings = {
+      url : currentWeatherUrl,
       dataType : "jsonp",
-      success : function(parsed_json) {
-        resolve(parsed_json);
+      success: function(parsed_json) {
+        Ember.run(null, resolve(parsed_json));
+      },
+      error: function (error_thrown) {
+        Ember.run(null, reject(error_thrown));
       }
-    });
+    };
+
+    Ember.$.ajax(settings);
   });
 }
+
+// function getForecastData() {
+//   if (FAKEWEATHERSERVER) { return fakeWeatherData; }
+
+//   return new Ember.RSVP.Promise(function (resolve, reject) {
+//     Ember.$.ajax({
+//       url : 'http://api.wunderground.com/api/' + apiKeys.wunderground + '/geolookup/conditions/q/WA/Seattle.json',
+//       dataType : "jsonp",
+//       success : function(parsed_json) {
+//         resolve(parsed_json);
+//       }
+//     });
+//   });
+// }
 
 ajax.defineFixture('/weather', {
   response: 'some weather data',
@@ -35,7 +64,8 @@ ajax.defineFixture('/image', {
 export default Ember.Route.extend({
   model: function () {
     var promises = {
-      weatherData: getWeatherData(),
+      weatherData: getCurrentWeatherData(),
+      // weatherForcastData: getForecastData(),
       imageData: ajax('/image')
     };
 
