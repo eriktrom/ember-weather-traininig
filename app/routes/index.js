cb4726b6752c42035f91734b26d91032dde2cdbf
Erik Trom <erik.trom@gmail.com>
2013-12-06 00:56:32 -0800

    passing data returned from a route to the component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,8 @@
 export default Ember.Route.extend({
-
+  model: function () {
+    // later we'll use this hook to grab weather data from *somewhere*
+    // or we'll realize that the index route hook won't allow query params
+    // so we'll move this.
+    return 'some usefule data from an api';
+  }
 });
